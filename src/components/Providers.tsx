efe19917e6f693f33ad1f497d9c8d03858f638b3
Daniel Sakyi <dannysakyi@gmail.com>
2025-09-07 14:58:39 -0400

    changes

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,7 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { SessionProvider } from "next-auth/react"
-
+// 
 const Providers = ({ children }: { children: React.ReactNode }) => {
 
     const queryClient = new QueryClient()
